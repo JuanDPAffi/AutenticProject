4781feca294706838954780f717bf327f546b9c0
JuanDPAffi <juan.pinilla@affi.net>
2025-07-16 10:30:39 -0500

    correccion ruta

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.js";
-import connectDB from "./config/db.js"; 
+import connectDB from "./db.js"; 
 
 // Cargar variables de entorno desde .env
 dotenv.config();
